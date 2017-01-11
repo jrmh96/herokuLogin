471e46fa7e7a42ce79d1e7ca0a1baceaaaa4d6da
jeremyhu <jrmh96@gmail.com>
2017-01-11 18:37:27 -0500

    logout (please work)

--- routes/index.js
+++ routes/index.js
@@ -5,7 +5,7 @@ var User = require("../models/users");
 
 // GET /logout
 
-router.get('logout', function(req, res, next){
+router.get('/logout', function(req, res, next){
     if(req.session) {
         //delete session object
         req.session.destroy(function(err){
